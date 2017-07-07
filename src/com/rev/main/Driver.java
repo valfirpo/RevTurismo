be303982908142c0536f336236ef192e9beec0bc
pwess <paulwesson27@gmail.com>
2017-07-07 17:27:29 -0400

    test

--- src/com/rev/main/Driver.java
+++ src/com/rev/main/Driver.java
@@ -5,9 +5,9 @@ import com.rev.bean.JenkinsTest;
 public class Driver {
 	public static void main(String [] args) {
 		JenkinsTest jenks = new JenkinsTest();
-		jenks.setOld("old");
+		jenks.setOld("bald");
 		jenks.setAge(90000);
-		jenks.setButler("pimp!");
+		jenks.setButler("Jeffery!");
 		
 		System.out.println("Jenkins is extremely " + jenks.getOld() + "!" + " Jenkins is " + jenks.getAge()+ " years old!" + " Jenkins is also a " + jenks.getButler());
 	}
