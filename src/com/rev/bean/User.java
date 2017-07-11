bbbf6990b6396a5f17b47dc5d863a7a9d18e5112
Valentin Firpo <valfirpo90@live.com>
2017-07-10 22:23:19 -0400

    user fixed

--- src/com/rev/bean/User.java
+++ src/com/rev/bean/User.java
@@ -1,60 +1,6 @@
 package com.rev.bean;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-public class User {
-  private String username;
-  private String password;
-  private String firstname;
-  private String lastname;
-  private String email;
-  private String address;
-  private int phone;
-  public String getUsername() {
-  return username;
-  }
-  public void setUsername(String username) {
-  this.username = username;
-  }
-  public String getPassword() {
-  return password;
-  }
-  public void setPassword(String password) {
-  this.password = password;
-  }
-  public String getFirstname() {
-  return firstname;
-  }
-  public void setFirstname(String firstname) {
-  this.firstname = firstname;
-  }
-  public String getLastname() {
-  return lastname;
-  }
-  public void setLastname(String lastname) {
-  this.lastname = lastname;
-  }
-  public String getEmail() {
-  return email;
-  }
-  public void setEmail(String email) {
-  this.email = email;
-  }
-  public String getAddress() {
-  return address;
-  }
-  public void setAddress(String address) {
-  this.address = address;
-  }
-  public int getPhone() {
-  return phone;
-  }
-  public void setPhone(int phone) {
-  this.phone = phone;
-  }
-=======
-=======
->>>>>>> master
+
 public class User 
 {
 	private int id;
@@ -160,8 +106,5 @@ public class User
 		return "User [id=" + id + ", username=" + username + ", password=" + password + ", firstname=" + firstname
 				+ ", lastname=" + lastname + ", email=" + email + ", cash=" + cash + ", urId=" + urId + "]";
 	}
-<<<<<<< HEAD
->>>>>>> refs/heads/master
-=======
->>>>>>> master
+
 }
