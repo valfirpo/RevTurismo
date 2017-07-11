b4836704666fca6437b78c5843fdf45440120d49
pwess <paulwesson27@gmail.com>
2017-07-10 22:36:27 -0400

    no erros

--- src/com/rev/dao/UserDao.java
+++ src/com/rev/dao/UserDao.java
@@ -1,10 +1,6 @@
 package com.rev.dao;
 
-import com.rev.bean.Login;
-import com.rev.bean.User;
-
 public interface UserDao {
 	  void register(User user);
 	  User validateUser(Login login);
-	  
 	}
