88dc5b6fac4765dce7865eac041a33d16d2f198b
Matthew Young <rainick987@gmail.com>
2017-07-11 10:14:29 -0400

    Web and pom xml

--- src/com/rev/controller/RegistrationController.java
+++ src/com/rev/controller/RegistrationController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.rev.bean.User;
-import com.revature.service.UserService;
+import com.rev.service.UserService;
 
 @Controller
 public class RegistrationController {
