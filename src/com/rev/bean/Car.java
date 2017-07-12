477a8f4e9b0e50508513d9f4f1fb0473acce2826
Matthew Young <rainick987@gmail.com>
2017-07-11 23:39:54 -0400

    Getting user's car dependency to work

--- src/com/rev/bean/Car.java
+++ src/com/rev/bean/Car.java
@@ -1,10 +1,15 @@
 package com.rev.bean;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
@@ -33,6 +38,8 @@ public class Car {
 	private double acceleration;
 	@Column(name = "C_HANDLING")
 	private double handling;
+	
+	
 	public Car(){
 		
 	}
