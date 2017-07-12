52672261b141e8252eac1f6fd3645d9c4e7b9424
Matthew Young <rainick987@gmail.com>
2017-07-12 11:17:16 -0400

    FrontController: Added challenges (List<Challenge>) to request, Added cars (List<Car>) to request.

--- src/com/rev/controller/FrontController.java
+++ src/com/rev/controller/FrontController.java
@@ -2,37 +2,41 @@ package com.rev.controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.rev.bean.User;
+import com.rev.dao.CarDao;
+import com.rev.dao.CarDaoImpl;
+import com.rev.dao.ChallengeDao;
+import com.rev.dao.ChallengeDaoImpl;
 import com.rev.service.UserService;
 
-
 @Controller
-@RequestMapping(value="/")
+@RequestMapping(value = "/")
 public class FrontController {
 
-  @Autowired
-  UserService userService;
-	  
-	  
-  @RequestMapping(value="controlPanel")
-  public String controlPaneln(HttpServletRequest request, HttpServletResponse response) {
-    return "controlPanel";
-  }
-  
-  @RequestMapping(value="viewCars")
-  public String viewCars(HttpServletRequest request, HttpServletResponse response) {
-    return "viewCars";
-  }
-  @RequestMapping(value="viewChallenges")
-  public String viewChallenges(HttpServletRequest request, HttpServletResponse response) {
-    return "viewChallenges";
-  }
-  
+	@Autowired
+	UserService userService;
+
+	@RequestMapping(value = "controlPanel")
+	public String controlPaneln(HttpServletRequest request, HttpServletResponse response) {
+		return "controlPanel";
+	}
+
+	@RequestMapping(value = "viewCars")
+	public String viewCars(HttpServletRequest request, HttpServletResponse response) {
+		CarDao dao = new CarDaoImpl();
+		request.setAttribute("cars", dao.getCars());
+		return "viewCars";
+	}
+
+	@RequestMapping(value = "viewChallenges")
+	public String viewChallenges(HttpServletRequest request, HttpServletResponse response) {
+		ChallengeDao dao = new ChallengeDaoImpl();
+		request.setAttribute("challenges", dao.getChallenges());
+		return "viewChallenges";
+	}
+
 }
